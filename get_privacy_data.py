a9405994e4298158bd01fd208519f3dae30bbd6e
Noah Finberg <noahfinberg@gmail.com>
2021-02-22 09:34:29 -0500

    check to see if it's feasible to get blacklight privacy data (TLDR- it is).

---
+++ get_privacy_data.py
@@ -0,0 +1,33 @@
+import json
+import requests
+
+# note: url must be in form http://{example.com}
+def get_blacklight_privacy_data(url):
+    blacklight_endpoint = 'https://blacklight.api.themarkup.org'
+    data = { "inUrl": url }
+
+    r = requests.post(url=blacklight_endpoint, json=data)
+    result = r.json()
+    return result
+
+def write_json_to_file(json_data, file_path):
+    with open(file_path, 'a+') as json_file:
+        json.dump(json_data, json_file)
+
+
+
+test_websites = [
+    'google.com',
+    'facebook.com',
+    'twitter.com',
+    'nytimes.com',
+    'foxnews.com'
+]
+
+
+
+for tw in test_websites:
+    test_file_path = "data/{}".format(tw)
+    url = "http://" + tw
+    blacklight_json_data = get_blacklight_privacy_data(url)
+    write_json_to_file(blacklight_json_data, test_file_path)
